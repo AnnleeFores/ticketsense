2b59e8c457be24dd61883b3e9b1321637b4fe15e
AnnleeFores <annleefores@gmail.com>
2021-12-08 01:14:35 +0530

    .

--- ticketsense.py
+++ ticketsense.py
@@ -5,44 +5,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
-from time import sleep
-from twilio.rest import Client
+import os
+import telebot
+
 
 #Enter BookMyShow and TicketNew theater links here inside single quotes seperated by comma - Make sure to remove date from the end
 
 links = ['https://in.bookmyshow.com/buytickets/carnival-downtown-thalassery/cinema-thay-CDTH-MT',
 'https://in.bookmyshow.com/buytickets/aura-cinema-mattannur/cinema-matt-ACMR-MT',
 'https://www.ticketnew.com/Carnival-Downtown--Thalassery-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/12539',
-#https://www.ticketnew.com/Liberty-Paradise-Complex--Thalassery-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/1203',
-#https://www.ticketnew.com/Mallika-Plex-Dolby-Atmos--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/10264',
+'https://www.ticketnew.com/Liberty-Paradise-Complex--Thalassery-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/1203',
+'https://www.ticketnew.com/Mallika-Plex-Dolby-Atmos--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/10264',
 'https://in.bookmyshow.com/buytickets/carnival-arti-suncity-mall-barasat/cinema-kolk-ACBK-MT',
 'https://in.bookmyshow.com/buytickets/pvr-lulu-kochi/cinema-koch-PVKC-MT',
 'https://www.ticketnew.com/Apsara-Theatre-4K--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/10515',
-#https://www.ticketnew.com/Crown-Theatre-Dolby-Atmos--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/213'
+'https://www.ticketnew.com/Crown-Theatre-Dolby-Atmos--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/213'
 ]
 
 
 browser = webdriver.Firefox() #opens web browser -> firefox
 
-# DATE of booking
-main_DATE = '16' 
-Mon = '12'
-Year = '2021'
+# Date of booking
+
+DATE = '16' 
+MON = '12'
+YEAR = '2021'
 
 filmname = 'spider' #first word of film name
 
-client = Client()
-from_whatsapp_number='whatsapp:+14155238886'
-to_whatsapp_number='whatsapp:+918129250447'
+#Telegram bot code
+API_KEY = os.getenv('API_KEY')
+USER_ID = os.getenv('USER_ID')
+bot = telebot.TeleBot(API_KEY)
 
 def message(msg):
-    client.messages.create(body=msg,
-                       from_=from_whatsapp_number,
-                       to=to_whatsapp_number)
+    bot.send_message(USER_ID, msg)
+
 
 def senseticket_bms(arg):
     
-    browser.get(arg + f'/{Year}{Mon}{main_DATE}')
+    browser.get(arg + f'/{YEAR}{MON}{DATE}')
     try:
         date = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.ID, 'showDates')))
@@ -57,25 +59,25 @@ def senseticket_bms(arg):
         pp = po.search(date.text)
         p = pp.group()
         
-        if p == main_DATE:
-            print(f'Bookmyshow: {venue.text} {main_DATE}th Dec slot opened!!!')
+        if p == DATE:
+            print(f'Bookmyshow: {venue.text} {DATE}th Dec slot opened!!!')
             for count, show in enumerate(showsB, start=1):
                 print(count, f'- Ticket booking started for {show.text}')
                 if filmname in show.text.lower():
-                    print(f'Found Spidey - {arg}/{Year}{Mon}{main_DATE}')
-                    message(f'Found Spidey - {arg}/{Year}{Mon}{main_DATE}')
+                    print(f'Found Spidey - {arg}/{YEAR}{MON}{DATE}')
+                    message(f'Found Spidey - {arg}/{YEAR}{MON}{DATE}')
                     
             print('-'.center(80, '-'))
         else:
             print(f'Bookmyshow: {venue.text} not yet open')
             print('-'.center(80, '-'))
     except:
-        print('Was not able to find an element with that name.')
+        print('Bookmyshow: Was not able to find an element with that name.')
         print('-'.center(80, '-'))
 
 
 def senseticket_tnew(arg):
-    browser.get(arg + f'/202112{main_DATE}')
+    browser.get(arg + f'/202112{DATE}')
     try:
         venue = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="divTheatreInfo"]/h2')))
@@ -92,20 +94,20 @@ def senseticket_tnew(arg):
         qp = qo.search(date.text)
         q = qp.group()
 
-        if q == main_DATE:
-            print(f'Ticket New: {venue.text} {main_DATE}th Dec slot opened!!!')
+        if q == DATE:
+            print(f'Ticket New: {venue.text} {DATE}th Dec slot opened!!!')
             for count, show in enumerate(showsT[1:], start=1):
                 print(count,f'- Ticket booking started for {show.text}')
                 if filmname in show.text.lower():
-                    print(f'Found Spidey - {arg}/{Year}{Mon}{main_DATE}')
-                    message(f'Found Spidey - {arg}/{Year}{Mon}{main_DATE}')
+                    print(f'Found Spidey - {arg}/{YEAR}{MON}{DATE}')
+                    message(f'Found Spidey - {arg}/{YEAR}{MON}{DATE}')
             print('-'.center(80, '-'))
         else:
             print(f'Ticket New: {venue.text} not yet open')
             print('-'.center(80, '-'))
 
     except:
-        print(f'Ticket New:{venue.text} Was not able to find an element with that name.')
+        print(f'Ticket New: Was not able to find an element with that name.')
         print('-'.center(80, '-'))
 
 
