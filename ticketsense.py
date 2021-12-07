b2a849987661463fd65bd776e17df660703ae3ce
AnnleeFores <annleefores@gmail.com>
2021-12-07 17:06:59 +0530

    speed

--- ticketsense.py
+++ ticketsense.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
 
@@ -22,9 +25,15 @@ def senseticket_bms(arg):
     
     browser.get(arg)
     try:
-        date = browser.find_element_by_id('showDates')
-        venue = browser.find_element_by_css_selector('a.venue-heading')
-        showsB = browser.find_elements_by_css_selector('a.nameSpan')
+        date = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, 'showDates')))
+
+        venue = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'a.venue-heading')))
+
+        showsB = WebDriverWait(browser, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.nameSpan')))
+
         p = (date.text[0:2])
         if p == main_DATE:
             print(f'Bookmyshow: {venue.text} {main_DATE}th Dec slot opened!!!')
@@ -43,9 +52,15 @@ def senseticket_bms(arg):
 def senseticket_tnew(arg):
     browser.get(arg)
     try:
-        venue = browser.find_element_by_xpath('//*[@id="divTheatreInfo"]/h2')
-        showsT = browser.find_elements_by_class_name('tn-entity-details')
-        date = browser.find_element_by_css_selector('li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active')
+        venue = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="divTheatreInfo"]/h2')))
+
+        showsT = WebDriverWait(browser, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'tn-entity-details')))
+
+        date = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active')))
+
         q = (date.text[4:])
         if q == main_DATE:
             print(f'Ticket New: {venue.text} {main_DATE}th Dec slot opened!!!')
@@ -68,22 +83,18 @@ def listToString(s):
 
     
 def loopy(argu):
-    
-    
     if bms_links == argu:
         print('\n')
         print('Starting - BookMyShow'.center(80, '-'))
         print('')
         for i in argu:
             senseticket_bms(i)
-            time.sleep(5)
     else:
         print('\n')
         print('Starting - Ticket New'.center(80, '-'))
         print('')
         for i in argu:
             senseticket_tnew(i)
-            time.sleep(5)
 
 
 links.sort()
