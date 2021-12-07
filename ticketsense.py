b11819d8e501aaa22e1538606dbab3c4c33ff428
AnnleeFores <annleefores@gmail.com>
2021-12-07 16:51:56 +0530

    .

--- ticketsense.py
+++ ticketsense.py
@@ -8,13 +8,14 @@ links = ['https://in.bookmyshow.com/buytickets/carnival-downtown-thalassery/cine
 'https://in.bookmyshow.com/buytickets/aura-cinema-mattannur/cinema-matt-ACMR-MT/20211216',
 'https://www.ticketnew.com/Carnival-Downtown--Thalassery-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/12539/20211216',
 'https://www.ticketnew.com/Liberty-Paradise-Complex--Thalassery-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/1203/20211216',
-'https://www.ticketnew.com/Mallika-Plex-Dolby-Atmos--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/10264/20211216'
+'https://www.ticketnew.com/Mallika-Plex-Dolby-Atmos--Calicut-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/10264/20211216',
+'https://in.bookmyshow.com/buytickets/carnival-arti-suncity-mall-barasat/cinema-kolk-ACBK-MT/20211216'
 ]
 
 
 browser = webdriver.Firefox() #opens web browser -> firefox
 
-DATE = '16'
+main_DATE = '16' # DATE of booking
 
 
 def senseticket_bms(arg):
@@ -23,36 +24,41 @@ def senseticket_bms(arg):
     try:
         date = browser.find_element_by_id('showDates')
         venue = browser.find_element_by_css_selector('a.venue-heading')
-        shows = browser.find_elements_by_css_selector('a.nameSpan')
+        showsB = browser.find_elements_by_css_selector('a.nameSpan')
         p = (date.text[0:2])
-        if p == DATE:
-            print(f'Bookmyshow: {venue.text} 16th Dec slot opened!!!')
-            for show in shows:
+        if p == main_DATE:
+            print(f'Bookmyshow: {venue.text} {main_DATE}th Dec slot opened!!!')
+            for show in showsB:
                 print(f'Ticket booking started for {show.text}')
-            print('')
+            print('-'.center(80, '-'))
         else:
             print(f'Bookmyshow: {venue.text} not yet open')
-            print('')
-        return shows
+            print('-'.center(80, '-'))
+        return showsB
     except:
         print('Was not able to find an element with that name.')
-        print('')
+        print('-'.center(80, '-'))
 
 
 def senseticket_tnew(arg):
     browser.get(arg)
     try:
-        elem = browser.find_element_by_css_selector('li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active')
-        q = (elem.text[4:])
-        print(f'{q} Dec')
-        if q == '16':
-            print('Ticket New: Spidey day slot opened!!!')
+        venue = browser.find_element_by_xpath('//*[@id="divTheatreInfo"]/h2')
+        showsT = browser.find_elements_by_class_name('tn-entity-details')
+        date = browser.find_element_by_css_selector('li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active')
+        q = (date.text[4:])
+        if q == main_DATE:
+            print(f'Ticket New: {venue.text} {main_DATE}th Dec slot opened!!!')
+            for show in showsT[1:]:
+                print(f'Ticket booking started for {show.text}')
+            print('-'.center(80, '-'))
         else:
-            print('Ticket New: not yet open')
-        return q
+            print(f'Ticket New: {venue.text} not yet open')
+            print('-'.center(80, '-'))
+        return showsT
     except:
-        print('Was not able to find an element with that name.')
-
+        print(f'Ticket New:{venue.text} Was not able to find an element with that name.')
+        print('-'.center(80, '-'))
 
 def listToString(s):
     str1 = "" 
@@ -62,11 +68,19 @@ def listToString(s):
 
     
 def loopy(argu):
+    
+    
     if bms_links == argu:
+        print('\n')
+        print('Starting - BookMyShow'.center(80, '-'))
+        print('')
         for i in argu:
             senseticket_bms(i)
             time.sleep(5)
     else:
+        print('\n')
+        print('Starting - Ticket New'.center(80, '-'))
+        print('')
         for i in argu:
             senseticket_tnew(i)
             time.sleep(5)
@@ -84,6 +98,6 @@ bms_links = links[:lenvalue]
 tnew_links = links[lenvalue:]
 
 loopy(bms_links)
-#loopy(tnew_links)
+loopy(tnew_links)
 
 browser.quit()
