759392535d12778b7a0bc86593cdba24e629d098
Annlee Fores <annleefores@gmail.com>
2021-12-08 14:48:44 +0000

    .

--- ticketsense.py
+++ ticketsense.py
@@ -54,12 +54,9 @@ filmname = 'spider'  #first word of film name
 #Telegram bot code
 load_dotenv()
 
-#API_KEY = os.environ['API_KEY']
-#USER_ID = os.environ['USER_ID']
 API_KEY = os.getenv('API_KEY')
 USER_ID = os.getenv('USER_ID')
 bot = telebot.TeleBot(API_KEY)
-
 
 def message(msg):
     bot.send_message(USER_ID, msg)
